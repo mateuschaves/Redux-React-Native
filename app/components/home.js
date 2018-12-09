06984aaf506234baaa05270e9af20af8afdffebd
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-12-09 04:56:44 -0300

    Aeewww

--- app/components/home.js
+++ app/components/home.js
@@ -12,7 +12,7 @@ import {
 import {bindActionCreators} from 'redux';
 import { connect } from 'react-redux';
 
-import * as Actions from '../actions'; //Import your actions
+import * as Actions from '../actions';
 
 class Home extends Component {
     constructor(props) {
@@ -25,7 +25,7 @@ class Home extends Component {
     }
 
     componentDidMount() {
-        this.props.getData(); //call our action
+        this.props.getData(); 
     }
 
     render() {
@@ -63,11 +63,6 @@ class Home extends Component {
     }
 };
 
-
-
-// The function takes data from the app current state,
-// and insert/links it into the props of our component.
-// This function makes Redux know that this component needs to be passed a piece of the state
 function mapStateToProps(state, props) {
     return {
         loading: state.dataReducer.loading,
@@ -75,14 +70,10 @@ function mapStateToProps(state, props) {
     }
 }
 
-// Doing this merges our actions into the component’s props,
-// while wrapping them in dispatch() so that they immediately dispatch an Action.
-// Just by doing this, we will have access to the actions defined in out actions file (action/home.js)
 function mapDispatchToProps(dispatch) {
     return bindActionCreators(Actions, dispatch);
 }
 
-//Connect everything
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
 
 const styles = StyleSheet.create({
