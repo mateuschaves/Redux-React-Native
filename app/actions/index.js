c99ff3268a7108572aabff4ce9fb7cc2f3c8406d
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-12-09 03:29:04 -0300

    Create action

---
+++ app/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const DATA_AVAILABLE = 'DATA_AVAILABLE';
+export const DATA_UNAVAILABLE = 'DATA_UNAVAILABLE';
+
+const baseURL = 'https://jsonplaceholder.typicode.com/';
+
+
+
+export function getData(){
+    return (dispatch) => {
+        const request = await axios.get(`${baseURL}/posts`);
+        if(request.status > 400){
+            dispatch({type: DATA_UNAVAILABLE, data: null});
+        }else{
+            dispatch({type:DATA_AVAILABLE, data: request.data});
+        }
+    }
+}
